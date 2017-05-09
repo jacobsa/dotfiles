5275e282d653e5a74922ba8b96a4dab5ab4f3c7c
Aaron Jacobs <aaronjjacobs@gmail.com>
2017-05-09 13:33:09 +1000

    config.fish: call out to google-specific.fish when it exists.

--- config.fish
+++ config.fish
@@ -1,2 +1,7 @@
 # Enable vim mode.
 fish_vi_key_bindings
+
+# Set up Google-specific things when at Google.
+if test -e ~/.google-dotfiles/google-specific.fish
+  . ~/.google-dotfiles/google-specific.fish
+end
